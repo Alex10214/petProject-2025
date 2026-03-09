db5a6ee4ba489ffc1a4cf06c42bcaec2c4ee2527
Alex10214 <10214q@gmail.com>
2026-03-09 16:30:40 +0200

    fixed error interceptor

--- client/src/core/interceptors/error-interceptor.ts
+++ client/src/core/interceptors/error-interceptor.ts
@@ -10,34 +10,35 @@ export const errorInterceptor: HttpInterceptorFn = (req, next) => {
   const router = inject(Router);
   return next(req).pipe(
     catchError(error => {
-      if (error) {
-        switch (error.status) {
-          case 400:
-            if (error.error.errors) {
-              const modelStateErrors = [];
-              for (const key in error.error.errors) {
-                if (error.error.errors[key]) {
-                  modelStateErrors.push(error.error.errors[key])
-                }
+      switch (error.status) {
+        case 400:
+          if (error.error?.errors) {
+            const modelStateErrors = [];
+            for (const key in error.error.errors) {
+              if (error.error.errors[key]) {
+                modelStateErrors.push(error.error.errors[key]);
               }
-              throw modelStateErrors.flat();
-            } else {
-              toastService.error(error.error)
             }
-            break;
-          case 401:
-            toastService.error('Unauthorized');
-            break;
-          case 404:
-            router.navigateByUrl('/not-found');
-            break;
-          case 500:
-            toastService.error('Server error. Please try again later.');
-            break;
-          default:
-            toastService.error('Something went wrong');
-            break;
-        }
+            throw modelStateErrors.flat();
+          } else {
+            toastService.error(error.error?.message || error.error || 'Bad request');
+          }
+          break;
+        case 401:
+          toastService.error('Unauthorized');
+          break;
+        case 403:
+          toastService.error('Access denied');
+          break;
+        case 404:
+          router.navigateByUrl('/not-found');
+          break;
+        case 500:
+          toastService.error('Server error. Please try again later.');
+          break;
+        default:
+          toastService.error('Something went wrong');
+          break;
       }
       throw error;
     })
